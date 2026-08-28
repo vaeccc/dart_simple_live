8a881fb2f76a299e345f7c541ef688cb25b8a483
Toloung <1172535606@qq.com>
2026-08-28 15:53:26 +0800

    feat: use local network sync only

--- simple_live_app/lib/modules/sync/sync_page.dart
+++ simple_live_app/lib/modules/sync/sync_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:remixicon/remixicon.dart';
 import 'package:simple_live_app/app/app_style.dart';
-import 'package:simple_live_app/app/utils.dart';
 import 'package:simple_live_app/routes/route_path.dart';
 import 'package:simple_live_app/widgets/settings/settings_card.dart';
 
+/// Synchronize with a device on the same local network.
 class SyncPage extends StatelessWidget {
   const SyncPage({super.key});
 
@@ -14,24 +13,19 @@ class SyncPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("数据同步"),
+        title: const Text('数据同步'),
         actions: [
           Visibility(
             visible: GetPlatform.isAndroid || GetPlatform.isIOS,
             child: TextButton.icon(
               onPressed: () async {
-                var result = await Get.toNamed(RoutePath.kSyncScan);
-                if (result == null || result.isEmpty) {
-                  return;
-                }
-                if (result.length == 5) {
-                  Get.toNamed(RoutePath.kRemoteSyncRoom, arguments: result);
-                } else {
+                final result = await Get.toNamed(RoutePath.kSyncScan);
+                if (result is String && result.isNotEmpty) {
                   Get.toNamed(RoutePath.kLocalSync, arguments: result);
                 }
               },
               icon: const Icon(Remix.qr_scan_line),
-              label: const Text("扫一扫"),
+              label: const Text('扫一扫'),
             ),
           ),
         ],
@@ -41,85 +35,15 @@ class SyncPage extends StatelessWidget {
         children: [
           Padding(
             padding: AppStyle.edgeInsetsA12.copyWith(top: 0),
-            child: Text(
-              "远程同步",
-              style: Get.textTheme.titleSmall,
-            ),
+            child: Text('局域网同步', style: Get.textTheme.titleSmall),
           ),
           SettingsCard(
-            child: Column(
-              children: [
-                ListTile(
-                  title: const Text("创建房间"),
-                  leading: const Icon(Remix.home_wifi_line),
-                  subtitle: const Text("其他设备可以通过房间号加入"),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    Get.toNamed(RoutePath.kRemoteSyncRoom);
-                  },
-                ),
-                AppStyle.divider,
-                ListTile(
-                  title: const Text("加入房间"),
-                  leading: const Icon(Remix.add_circle_line),
-                  subtitle: const Text("加入其他设备创建的房间"),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () async {
-                    var input = await Utils.showEditTextDialog(
-                      "",
-                      title: "加入房间",
-                      hintText: "请输入房间号,不区分大小写",
-                      validate: (text) {
-                        if (text.isEmpty) {
-                          SmartDialog.showToast("房间号不能为空");
-                          return false;
-                        }
-                        if (text.length != 5) {
-                          SmartDialog.showToast("请输入5位房间号");
-                          return false;
-                        }
-                        return true;
-                      },
-                    );
-                    if (input != null && input.isNotEmpty) {
-                      Get.toNamed(RoutePath.kRemoteSyncRoom,
-                          arguments: input.toUpperCase());
-                    }
-                  },
-                ),
-                AppStyle.divider,
-                ListTile(
-                  title: const Text("WebDAV"),
-                  leading: const Icon(Icons.cloud_upload_outlined),
-                  subtitle: const Text("通过WebDAV同步数据"),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    Get.toNamed(RoutePath.kRemoteSyncWebDav);
-                  },
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: AppStyle.edgeInsetsA12.copyWith(top: 24),
-            child: Text(
-              "局域网同步",
-              style: Get.textTheme.titleSmall,
-            ),
-          ),
-          SettingsCard(
-            child: Column(
-              children: [
-                ListTile(
-                  title: const Text("局域网同步"),
-                  subtitle: const Text("在局域网内同步数据"),
-                  leading: const Icon(Remix.device_line),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    Get.toNamed(RoutePath.kLocalSync);
-                  },
-                ),
-              ],
+            child: ListTile(
+              title: const Text('局域网同步'),
+              subtitle: const Text('连接同一网络内的电视或手机，同步数据'),
+              leading: const Icon(Remix.device_line),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () => Get.toNamed(RoutePath.kLocalSync),
             ),
           ),
         ],
