24fc7d5569d2917a55f7ed7d70381079b45f7f98
Toloung <1172535606@qq.com>
2026-09-01 11:26:43 +0800

    test: replace stale app counter smoke test

--- simple_live_app/test/widget_test.dart
+++ simple_live_app/test/widget_test.dart
@@ -5,26 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:simple_live_app/app/controller/app_settings_controller.dart';
 
 import 'package:simple_live_app/main.dart';
 
+class _TestAppSettingsController extends AppSettingsController {
+  @override
+  void onInit() {}
+}
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('application shell builds with configured settings', (
+    WidgetTester tester,
+  ) async {
+    Get.testMode = true;
+    Get.put<AppSettingsController>(_TestAppSettingsController());
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(GetMaterialApp), findsOneWidget);
+    Get.reset();
   });
 }
